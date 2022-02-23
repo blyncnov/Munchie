8f6a4f632a0209a7976e32393847afa6666577f5
blyncnov <bholuwatife00@gmail.com>
2022-02-23 23:50:33 +0100

    Munchie Food Ordering App fisrt commit v21

--- src/index.js
+++ src/index.js
@@ -7,10 +7,26 @@ import "./GlobalStyle.css";
 // import all Components
 import App from "./App";
 
+// https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3
+
 const Index = () => {
+  // useEffect(() => {
+  //   const PlayHandler = () => {};
+
+  //   window.addEventListener("load", PlayHandler);
+
+  //   return () => window.removeEventListener("load", PlayHandler);
+  // }, []);
+
   return (
     <>
       <App />
+      <audio loop id="backgroundMusic" controls autoPlay>
+        <source
+          src="http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/theme_01.mp3"
+          type="audio/mp3"
+        />
+      </audio>
     </>
   );
 };
