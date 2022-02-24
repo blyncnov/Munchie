1c182b5a07c8d78318a0a89d5091daf5bafbce9b
blyncnov <bholuwatife00@gmail.com>
2022-02-24 09:43:14 +0100

    Munchie Food Ordering App fisrt commit v22

--- src/style/HeaderStyle.js
+++ src/style/HeaderStyle.js
@@ -1,10 +1,15 @@
 import styled from "styled-components";
+import Bannertwo from "../assets/bannertwo.jpg";
 
 export const HeaderContainer = styled.div`
   width: 100%;
 
   /* background-color: rgb(221, 255, 154); */
   background-color: #ebe7ffb9;
+  background: url(${Bannertwo});
+  background-position: top, 20px;
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
 export default HeaderContainer;
